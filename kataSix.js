7fbc6d779f08a58469de0fa40768521414d5069c
Joseph Williams <josephwilliamsjs@gmail.com>
2020-11-06 17:02:16 +0000

    function that searches nested array for available parking spots depending on vehicle type

---
+++ kataSix.js
@@ -0,0 +1,70 @@
+// Kata 6 - SmartParking
+
+/*
+We need to write a function called whereCanIPark() that returns the coordinates of an available parking spot for the vehicle, or returns false if there is no available spot. 
+Our function receives an array of arrays representing parking spots, and a string with type of the vehicle that is looking for a parking spot.
+*/
+
+// uppcase means available lowcase means unavailable
+
+// to do
+// nested loop over spots arrays
+// check for relevant uppercase letter spots
+// if they find one return it [x, y] otherwise return false
+
+const whereCanIPark = function (spots, vehicle) {
+  for (let y = 0; y < spots.length; y++) {
+    for (let x = 0; x < spots[y].length; x++) {
+      if (vehicle === 'regular') {
+        if (spots[y][x] === 'R') {
+          return [x, y];
+        }
+      } else if (vehicle === 'small') {
+          if (spots[y][x] === 'R' || spots[y][x] === 'S') {
+            return [x, y];
+          }
+      } else if (vehicle === 'motorcycle') {
+        if (spots[y][x] === 'R' || spots[y][x] === 'S' || spots[y][x] === 'M') {
+          return [x, y];
+        }
+      }
+    }
+  }  
+  return false;
+};
+
+console.log(whereCanIPark(
+  [
+    // COLUMNS ARE X
+    // 0    1    2    3    4    5
+    ['s', 's', 's', 'S', 'R', 'M'], // 0 ROWS ARE Y
+    ['s', 'M', 's', 'S', 'r', 'M'], // 1
+    ['s', 'M', 's', 'S', 'r', 'm'], // 2
+    ['S', 'r', 's', 'm', 'r', 'M'], // 3
+    ['S', 'r', 's', 'm', 'r', 'M'], // 4
+    ['S', 'r', 'S', 'M', 'M', 'S']  // 5
+  ],
+  'regular'
+));
+
+console.log(whereCanIPark(
+  [
+    ['M', 'M', 'M', 'M'],
+    ['M', 's', 'M', 'M'],
+    ['M', 'M', 'M', 'M'],
+    ['M', 'M', 'r', 'M']
+  ],
+  'small'
+));
+
+console.log(whereCanIPark(
+  [
+    ['s', 's', 's', 's', 's', 's'],
+    ['s', 'm', 's', 'S', 'r', 's'],
+    ['s', 'm', 's', 'S', 'r', 's'],
+    ['S', 'r', 's', 'm', 'r', 's'],
+    ['S', 'r', 's', 'm', 'R', 's'],
+    ['S', 'r', 'S', 'M', 'm', 'S']
+  ],
+  'motorcycle'
+))
