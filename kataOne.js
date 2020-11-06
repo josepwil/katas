c9f39a4fde12e9e8f85ead55029e00d5a93bb7ab
Joseph Williams <josephwilliamsjs@gmail.com>
2020-11-06 01:23:40 +0000

    solved kata one returns sum of two highest numbers in given array

--- kataOne.js
+++ kataOne.js
@@ -6,9 +6,36 @@ We will then have to find the two largest numbers in that array, and sum them to
 */
 
 const sumLargestNumbers = function(data) {
-  // Put your solution here
+  let largest = 0;
+  let secondLargest = 0;
+  for (i = 0; i < data.length; i++) {
+    if (data[i] > largest) {
+      largest = data[i];
+    }
+  }
+  /* 
+  for (i = 0; i < data.length; i++) {
+    if (data[i] === largest) {
+      data.splice(i, 1);
+    }
+  }
+  */
+  for (i = 0; i < data.length; i++) {
+    if (data[i] > secondLargest && data[i] !== largest) {
+      secondLargest = data[i];
+    }
+  }
+  return largest + secondLargest;
+
 };
 
 console.log(sumLargestNumbers([1, 10]));
 console.log(sumLargestNumbers([1, 2, 3]));
 console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
+
+// iterate over array
+// find largest number 
+// remove largest number from array
+// iterate over again
+// find new largest number
+// return sum of those two numbers
