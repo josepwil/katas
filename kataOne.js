e6476cbe2bc0ba76f5637b8d32918762ed5153a5
Joseph Williams <josephwilliamsjs@gmail.com>
2020-11-06 01:04:07 +0000

    first kata: sums two largest numbers in a given array

---
+++ kataOne.js
@@ -0,0 +1,14 @@
+// Kata 1 - Sum the Largest Numbers
+
+/*
+In this exercise, we will be given an array of 2 or more numbers.
+We will then have to find the two largest numbers in that array, and sum them together.
+*/
+
+const sumLargestNumbers = function(data) {
+  // Put your solution here
+};
+
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
